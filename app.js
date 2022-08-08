30522362446cfdbbf35222645b2bc3c364ce120e
Manuel2u <edodoo011@st.ug.edu.gh>
2022-08-08 17:34:46 +0000

    updated files

--- app.js
+++ app.js
@@ -32,7 +32,17 @@ app.get("/contact", function(req, res){
     });
 });
 
+app.get("/compose", function(req, res){
+    res.render("compose");
+});
 
+
+app.post("/compose", function(req, res){
+  const post = { 
+    title : req.body.postTitle,
+    content : req.body.postBody,
+    }
+});
 
 
 
